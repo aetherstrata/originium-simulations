58ffe10d82959285347fad19957af268908c9cbf
Davide Pierotti <d.pierotti@live.it>
2023-06-25 01:45:10 +0200

    Add tailwindcss

---
+++ src/main/frontend/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme"
+
+module.exports = {
+  content: ["../resources/templates/**/*.{html,js}"],
+  theme: {
+    extend: {
+      width:{
+        "1/10": "10%"
+      },
+      spacing:{
+        112: "28rem",
+        128: "32rem",
+        256: "64rem"
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
+}
