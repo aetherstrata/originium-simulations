0b51049b3fd8ad8ce79c39b72ff41b92d29fcb52
Davide Pierotti <d.pierotti@live.it>
2023-06-25 01:43:14 +0200

    Initial commit

---
+++ src/main/java/dev/aest/ark/AestArk.java
@@ -0,0 +1,15 @@
+package dev.aest.ark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AestArk
+{
+    public static final String PROJECT_NAME = "aestArk";
+
+    public static void main(String[] args) {
+        SpringApplication.run(AestArk.class, args);
+    }
+
+}
---
+++ src/test/java/dev/aest/ark/AestArkTests.java
@@ -0,0 +1,14 @@
+package dev.aest.ark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AestArkTests
+{
+
+    @Test
+    void contextLoads() {
+    }
+
+}
