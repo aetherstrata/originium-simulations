da598ddf3c17d3bea95f5a69292930926da3c43a
Davide Pierotti <d.pierotti@live.it>
2023-06-29 17:19:48 +0200

    Return 404 for invalid API requests

--- src/main/java/dev/aest/ark/controller/RestApiController.java
+++ src/main/java/dev/aest/ark/controller/RestApiController.java
@@ -4,10 +4,12 @@ import dev.aest.ark.dto.FullItemDto;
 import dev.aest.ark.dto.ItemDto;
 import dev.aest.ark.service.ItemService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -20,11 +22,15 @@ public class RestApiController
 
     @GetMapping("items")
     public List<ItemDto> getAllItems(){
-        return itemService.getAllItems().stream().map(ItemDto::of).toList();
+        List<ItemDto> itemList = itemService.getAllItems().stream().map(ItemDto::of).toList();
+        if (itemList.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "There are no items in the database");
+        return itemList;
     }
 
     @GetMapping("item/{id}")
     public FullItemDto getItemDetails(@PathVariable("id") final Long id){
-        return FullItemDto.of(itemService.getFullItem(id));
+        FullItemDto dto = FullItemDto.of(itemService.getFullItem(id));
+        if (dto == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "This item does not exist");
+        return dto;
     }
 }
