12509d9fc11f4066e6fde3f2ea2002fb7ca06651
Davide Pierotti <d.pierotti@live.it>
2023-07-01 13:40:27 +0200

    Rplace @Data for @Getter ,@Setter and implement equals() hashCode()

--- src/main/java/dev/aest/ark/model/InventoryItem.java
+++ src/main/java/dev/aest/ark/model/InventoryItem.java
@@ -3,11 +3,12 @@ package dev.aest.ark.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.Hibernate;
 
-@Data
-@NoArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "inventory_items", uniqueConstraints = { @UniqueConstraint(columnNames = {"user_id", "item_id"})})
 public final class InventoryItem
@@ -16,18 +17,36 @@ public final class InventoryItem
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(optional = false)
     private User user;
 
-    @ManyToOne
+    @ManyToOne(optional = false)
     private Item item;
 
     @NotNull
-    @Positive
+    @Positive(message = "{number.positive}")
     private Integer quantity = 0;
 
     public InventoryItem(User user, Item item){
         this.user = user;
         this.item = item;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+
+        InventoryItem that = (InventoryItem) o;
+
+        if (!user.equals(that.user)) return false;
+        return item.equals(that.item);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = user.hashCode();
+        result = 31 * result + item.hashCode();
+        return result;
     }
 }
--- src/main/java/dev/aest/ark/model/PlannedItem.java
+++ src/main/java/dev/aest/ark/model/PlannedItem.java
@@ -3,11 +3,15 @@ package dev.aest.ark.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import lombok.Data;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.NoArgsConstructor;
+import org.hibernate.Hibernate;
 
-@Data
-@NoArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "planned_items", uniqueConstraints = { @UniqueConstraint(columnNames = {"user_id", "item_id"})})
 public final class PlannedItem
@@ -16,10 +20,10 @@ public final class PlannedItem
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(optional = false)
     private User user;
 
-    @ManyToOne
+    @ManyToOne(optional = false)
     private Item item;
 
     @NotNull
@@ -29,5 +33,23 @@ public final class PlannedItem
     public PlannedItem(User user, Item item){
         this.user = user;
         this.item = item;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+
+        PlannedItem that = (PlannedItem) o;
+
+        if (!user.equals(that.user)) return false;
+        return item.equals(that.item);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = user.hashCode();
+        result = 31 * result + item.hashCode();
+        return result;
     }
 }
